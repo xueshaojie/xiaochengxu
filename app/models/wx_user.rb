18fa5f572ad853279c52494b0042087cc2915feb
xueshaojie <xue.shaojie@biaotutech.com>
2017-08-24 19:20:24 +0800

    update

--- app/models/wx_user.rb
+++ app/models/wx_user.rb
@@ -3,13 +3,13 @@ class WxUser < ActiveRecord::Base
   has_one :user
 
 
-  after_create do
-    self.user.create(
-      email:  "xueshaojie12321@gmail.com",
-      password:  "123123123",
-      password_confirmation: "123123123"
-    )
-  end
+  # after_create do
+  #   self.user.create(
+  #     email:  "xueshaojie12321@gmail.com",
+  #     password:  "123123123",
+  #     password_confirmation: "123123123"
+  #   )
+  # end
 
 
 end
