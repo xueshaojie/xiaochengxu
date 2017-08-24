5dad5bdbcbf90a83a2d708c8e323b316ec30797f
xueshaojie <xue.shaojie@biaotutech.com>
2017-08-24 10:51:36 +0800

    update

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
 
-  http_basic_authenticate_with name: "xue", password:
-  "123123", only: :destroy 
+  # http_basic_authenticate_with name: "xue", password:
+  # "123123", only: :destroy
 
   def create
     @article = Article.find(params[:article_id])
